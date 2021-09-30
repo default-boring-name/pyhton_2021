5fc1d0d82262ff0d172e36cb423153dc641d5a28
default_boring_name <vasiakin.mm@phystech.edu>
2021-09-30 19:23:53 +0300

    Started to write a function for bamboo drawing.
    Completion status 1/3:
        Can draw a stem of bamboo

--- picture_for_exhibition/Task_2.py
+++ picture_for_exhibition/Task_2.py
@@ -1,25 +1,56 @@
 import pygame as pg
-import pygame.draw as pg_draw
 import math
+
+colors = {
+          'black': (0, 0, 0),
+          'white': (255, 255, 255),
+          'green': (0, 104, 55),
+          'peach': (255, 175, 128),
+          'None': (255, 230, 255)
+         }
+FPS = 30
+
+def draw_bamboo():
+    body = pg.Surface((780, 480))
+    body.set_colorkey(colors['None'])
+    body.fill(colors['None'])
+
+    stem = pg.Surface((100, 480))
+    stem.set_colorkey(colors['None'])
+    stem.fill(colors['None'])
+
+    stem_sect = [pg.Surface((37, 112)),
+                 pg.Surface((37, 127)),
+                 pg.Surface((25, 78)),
+                 pg.Surface((13, 105))]
+    
+    for i in range(4):
+        stem_sect[i].set_colorkey(colors['None'])
+        stem_sect[i].fill(colors['None'])
+        pg.draw.rect(stem_sect[i], colors['green'],
+        pg.Rect((0, 1), stem_sect[i].get_size()))
+
+    stem_sect[2] = pg.transform.rotate(stem_sect[2], -27)
+    stem_sect[3] = pg.transform.rotate(stem_sect[3], -27)
+
+    stem.blit(stem_sect[0], (5, 368))
+    stem.blit(stem_sect[1], (5, 231))
+    stem.blit(stem_sect[2], (0, 133))
+    stem.blit(stem_sect[3], (35, 8))
+
+    body.blit(stem, (375, 0))
+
+    return body 
 
 pg.init()
 
-FPS = 30
-window_size = (400, 400)
+window_size = (1200, 800)
 screen = pg.display.set_mode(window_size)
 
-colors = {
-         'black': (0, 0, 0),
-         'orange': (200, 100, 0),
-         'white': (255, 255, 255),
-         'green': (0, 255, 0)
-         }
+main_surf = pg.display.get_surface()
+main_surf.fill(colors['peach'])
 
-screen.fill(colors['orange'])
- 
-s = pg.Surface(100, 100)
-pg_draw.circle(s, colors['green'], (50, 50), 10)
-
+main_surf.blit(draw_bamboo(), (280, 230))
 
 pg.display.update()
 clock = pg.time.Clock()
@@ -32,4 +63,3 @@ while running:
             running = False
 
 pg.quit()
-
